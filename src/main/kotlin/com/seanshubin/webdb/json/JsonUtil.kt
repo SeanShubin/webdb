7ce51d32e4d908ae02418a28309deb5d9f90cab9
Sean Shubin <sshubin@cj.com>
2018-04-26 12:01:31 -0700

    json merge

--- src/main/kotlin/com/seanshubin/webdb/json/JsonUtil.kt
+++ src/main/kotlin/com/seanshubin/webdb/json/JsonUtil.kt
@@ -15,6 +15,9 @@ object JsonUtil {
         mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
     }
 
+    fun <T> toJson(theObject: T): String =
+            mapper.writeValueAsString(theObject)
+
     fun mapToJson(map: Map<String, Any>): String =
             mapper.writeValueAsString(map)
 
@@ -23,4 +26,42 @@ object JsonUtil {
 
     fun <T> fromJson(json: String, theClass: Class<T>): T =
             mapper.readValue(json, theClass)
+
+    fun normalize(json: String): String {
+        val asObject = fromJson(json, Object::class.java)
+        val normalized = toJson(asObject)
+        return normalized
+    }
+
+    fun mergeJsonStrings(a: String, b: String): String {
+        val aObject = JsonUtil.fromJson(a, Object::class.java)
+        val bObject = JsonUtil.fromJson(b, Object::class.java)
+        val cObject = JsonUtil.merge(aObject, bObject)
+        return toJson(cObject)
+    }
+
+    fun merge(aObject: Any?, bObject: Any?): Any? {
+        return when {
+            aObject is Map<*, *> && bObject is Map<*, *> -> mergeMaps(aObject, bObject)
+            else -> bObject
+        }
+    }
+
+    fun mergeMaps(aMap: Map<*, *>, bMap: Map<*, *>): Map<*, *> {
+        return bMap.entries.fold(aMap, { a, b -> mergeEntry(a, b) })
+    }
+
+    fun mergeEntry(aMap: Map<*, *>, bEntry: Map.Entry<*, *>): Map<*, *> {
+        val (bKey, bValue) = bEntry
+        return if (bValue == null) {
+            aMap - bKey
+        } else {
+            val aValue = aMap[bKey]
+            val cValue = when (aValue) {
+                null -> bValue
+                else -> merge(aValue, bValue)
+            }
+            aMap + Pair(bKey, cValue)
+        }
+    }
 }
