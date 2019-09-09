ddd4d6070b51b0456a3a8705f9e2be301271f77a
Sean Shubin <sshubin@cj.com>
2019-09-09 11:46:42 -0700

    Formatted source

--- console/src/main/kotlin/com/seanshubin/webdb/console/Application.kt
+++ console/src/main/kotlin/com/seanshubin/webdb/console/Application.kt
@@ -1,14 +1,14 @@
 package com.seanshubin.webdb.console
 
 import com.seanshubin.webdb.domain.Datum
-import com.seanshubin.webdb.domain.Id
-import com.seanshubin.webdb.domain.NamespaceId
-import com.seanshubin.webdb.domain.InMemoryDatabase
-import com.seanshubin.webdb.domain.JsonUtil.fromJson
-import com.seanshubin.webdb.domain.JsonUtil.toJson
 import com.seanshubin.webdb.domain.DbRoutes.IdRoute
 import com.seanshubin.webdb.domain.DbRoutes.NamespaceRoute
 import com.seanshubin.webdb.domain.DbRoutes.Root
+import com.seanshubin.webdb.domain.Id
+import com.seanshubin.webdb.domain.InMemoryDatabase
+import com.seanshubin.webdb.domain.JsonUtil.fromJson
+import com.seanshubin.webdb.domain.JsonUtil.toJson
+import com.seanshubin.webdb.domain.NamespaceId
 import io.ktor.application.Application
 import io.ktor.application.ApplicationCall
 import io.ktor.application.call
